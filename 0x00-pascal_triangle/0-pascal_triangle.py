571e09ded50779931db9974654c0ce4442f4dc34
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-04-09 19:49:33 +0000

    replaced factorial by comb

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -7,7 +7,8 @@ the Pascal’s triangle of n
 
 def pascal_triangle(n):
     """pascal triangle definition"""
-    from math import factorial
+    # from math import factorial
+    from math import comb
     if n <= 0:
         return []
     triangle = [[0] * (i + 1) for i in range(n)]
@@ -15,6 +16,7 @@ def pascal_triangle(n):
         triangle[i][0] = 1
         triangle[i][i] = 1
         for j in range(1, i):
-            triangle[i][j] = int(factorial(i)/(factorial(j) * factorial(i-j)))
+          triangle[i][j] = comb(i, j)
+            # triangle[i][j] = int(factorial(i)/(factorial(j) * factorial(i-j)))
 
     return triangle
