f09cb71623b13a8704f6409c0f6b02f6dedaaf79
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-05-21 20:50:44 +0000

    tried fixing code styling

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const request = require('request');
+const request = require("request");
 const movie_id = process.argv[2];
-const base_url = 'https://swapi-api.alx-tools.com/api/';
-const film_url = base_url + 'films/' + movie_id;
+const base_url = "https://swapi-api.alx-tools.com/api/";
+const film_url = base_url + "films/" + movie_id;
 
 function getCharacterName(charUrl) {
   return new Promise((resolve, reject) => {
@@ -15,9 +15,10 @@ function getCharacterName(charUrl) {
     });
   })
 }
+
 request({ url: film_url, json: true }, async (error, response, body) => {
   if (error) {
-    console.error('Error:', error);
+    console.error("Error:", error);
     return;
   }
   for (const character of body.characters) {
@@ -25,7 +26,7 @@ request({ url: film_url, json: true }, async (error, response, body) => {
       const name = await getCharacterName(character);
       console.log(name);
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
   }
 });
