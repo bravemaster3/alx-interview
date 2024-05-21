84f81da3a56f142334b7b7a08115a241b3f34bca
Koffi Dodji Noumonvi <39010414+bravemaster3@users.noreply.github.com>
2024-05-21 20:41:54 +0000

    changed with await async and promises

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,18 +4,28 @@ const movie_id = process.argv[2];
 const base_url = 'https://swapi-api.alx-tools.com/api/';
 const film_url = base_url + 'films/' + movie_id;
 
-request({ url: film_url, json: true }, (error, response, body) => {
+function getCharacterName(charUrl) {
+  return new Promise((resolve, reject) => {
+    request({ url: charUrl, json: true }, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(body.name);
+      }
+    });
+  })
+}
+request({ url: film_url, json: true }, async (error, response, body) => {
   if (error) {
     console.error('Error:', error);
     return;
   }
   for (const character of body.characters) {
-    request({ url: character, json: true }, (error, response, body) => {
-      if (error) {
-        console.error('Error:', error);
-        return;
-      }
-      console.log(body.name);
-    });
+    try {
+      const name = await getCharacterName(character);
+      console.log(name);
+    } catch (error) {
+      console.error('Error:', error);
+    }
   }
 });
