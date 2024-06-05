f4154acb307ef3c5d741a8c7656c5ce61c07b584
Koffi Dodji Noumonvi <bravemaster102@gmail.com>
2024-06-05 03:53:40 +0200

    Add 0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
+
 """
 makeChange function for computing the minimum number of coins to give change
 """
 
+
 def makeChange(coins, total):
     """function definition"""
-    if total <= 0:
-        return 0
 
-    # Create a list to store the fewest number of coins needed for each total
-    # from 0 to the given total
     dp = [float('inf')] * (total + 1)
-    dp[0] = 0  # Base case: 0 coins needed for total 0
+    dp[0] = 0
+
+    coins.sort(reverse=True)  # Sort coins in descending order
 
     for coin in coins:
         for amount in range(coin, total + 1):
             dp[amount] = min(dp[amount], dp[amount - coin] + 1)
 
-    return dp[total] if dp[total] != float('inf') else -1
+    return (dp[total] if dp[total] != float('inf') else -1)
